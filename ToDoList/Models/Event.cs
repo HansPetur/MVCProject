cc055741b0f7aa328019eee724add5948a9dd6e6
HansPetur <hanspmik@gmail.com>
2014-09-18 11:18:45 +0100

    Added Event Class

---
+++ ToDoList/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Event
+    {
+        public string eventName { get; set; }
+        [Required]
+        public string owner { get; set; }
+    }
+}
