142a238777cb6a63d9a4c990bbc3f82429975f29
HansPetur <hanspmik@gmail.com>
2014-09-18 12:03:09 +0100

    Added Controllers, Views

--- ToDoList/Models/Event.cs
+++ ToDoList/Models/Event.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ToDoList.Models
 {
     public class Event
     {
-        public string eventName { get; set; }
         [Required]
-        public string owner { get; set; }
+        [HiddenInput(DisplayValue=false)]
+        public int EventID{ get; set; }
+        [Required]
+        public string EventName { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+        [Required]
+        public DateTime Deadline { get; set; }
     }
 }
